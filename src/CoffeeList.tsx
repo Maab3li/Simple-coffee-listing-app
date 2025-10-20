80709f61e7b15c7e0b1abd63bd022f4d2fe912e9
Maab3li <maab3li555@gmail.com>
2025-10-20 16:12:34 -0700

    used tanstack query for data fetching and caching

--- src/CoffeeList.tsx
+++ src/CoffeeList.tsx
@@ -2,12 +2,12 @@ import type React from "react"
 import './App.css'
 import type { Coffee } from "./Card"
 import './index.css'
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import { useQuery } from "@tanstack/react-query"
 import { Card } from "./Card"
 
-export const CoffeeList:React.FC = () => {
 
-  const [coffeeData, setCoffeeData] = useState<Coffee[]>([])
+export const CoffeeList:React.FC = () => {
 
   const [isSelected, setIsSelected] = useState('all-products')
 
@@ -21,22 +21,22 @@ export const CoffeeList:React.FC = () => {
     }
   }
 
-  useEffect(() => {
-    fetch(
-  "https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/4-frontend-libaries/challenges/group_1/data/simple-coffee-listing-data.json"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    const coffees = (Object.values(data)) as Coffee[]
-    setCoffeeData(coffees)
-    console.log(coffeeData)
-    console.log(coffeeData[0])
+    const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['coffees'],
+    queryFn: async () => {
+      const response:Response = await fetch ("https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/4-frontend-libaries/challenges/group_1/data/simple-coffee-listing-data.json")
+      if(!response.ok) {
+        throw new Error('Error: ' +error)
+      }
+      return response.json()
+      
+    },
+    staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 60
   })
-  .catch((error) => {
-    console.log(error, 'An error occured while fetching coffee list')
-  });
-  }, [coffeeData])
 
+  if(isLoading) return <div>Loading...</div>
+  if(isError) return <div>Error: {error.message}</div>
 
   return (
     <div className='absolute flex flex-col bg-[#1B1D1F] *:font-dm-sans rounded-xl w-auto mt-20 pb-10 mx-5 lg:mt-40 lg:mx-10 xl:mx-25'>
@@ -51,14 +51,14 @@ export const CoffeeList:React.FC = () => {
             <button onClick={handleClick} id="available-now" className={`${isSelected == 'available-now'? 'bg-[#4D5562] hover:bg-[#6F757C] text-white  py-2 px-3 rounded-[7px]' : 'bg-transparent text-white'}`}>Available Now</button>
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-3 mx-auto mt-10 gap-10">
-          {isSelected == 'all-products' && coffeeData.map((coffee) => (
+          {isSelected == 'all-products' && data.map((coffee:Coffee) => (
             <Card 
             key={coffee.id}
             coffee={coffee}
             />
           ))}
-          {isSelected == 'available-now' && coffeeData.filter(availableCoffee => availableCoffee.available==true)
-          .map(coffee => (
+          {isSelected == 'available-now' && data.filter((availableCoffee:Coffee) => availableCoffee.available==true)
+          .map((coffee:Coffee) => (
             <Card 
             key={coffee.id}
             coffee={coffee}
