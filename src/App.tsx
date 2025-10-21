e309e14a7424601c0da2faa98e5ee572bab6f47c
Maab3li <maab3li555@gmail.com>
2025-10-21 09:55:37 -0700

    updated App height on different screen sizes

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App:React.FunctionComponent =() => {
   return (
     //main page wrapper
     <QueryClientProvider client={queryClient}>
-      <div className='relative grid grid-rows-4 grid-cols-1 max-w-[100%] h-[365vh] lg:h-[210vh] xl:h-[180vh] pb-10 bg-black'>
+      <div className='relative grid grid-rows-4 grid-cols-1 max-w-[100%] h-[320vh] lg:h-[180vh] pb-10 bg-black'>
         {/* background image */}
         <div className='w-full h-[50%] lg:h-[100%] bg-size-[100%_100%] bg-no-repeat bg-[url("assets/resources/bg-cafe-sm.jpg")] lg:bg-[url("assets/resources/bg-cafe-lg.jpg")] lg:bg-[url("assets/resources/bg-cafe-lg.jpg")]'></div>
         {/* coffee list container */}
