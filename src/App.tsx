2ea9cef56bdfca04327b41ede7d47042b3fa99f1
Maab3li <maab3li555@gmail.com>
2025-10-25 09:34:10 -0700

    added high fetchPriority to images, updated index.html file to load index.css asyncronously

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import type React from 'react'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 import { CoffeeList } from './CoffeeList'
+import smImage from '../src/assets/resources/bg-cafe-sm.jpg'
+import lgImage from '../src/assets/resources/bg-cafe-lg.jpg'
+import xlImage from '../src/assets/resources/bg-cafe.jpg'
 import './App.css'
 import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
 
@@ -25,7 +28,11 @@ const App:React.FunctionComponent =() => {
         {/*main page wrapper*/}
         <div className='relative grid grid-rows-4 grid-cols-1 max-w-[100%] h-[320vh] lg:h-[180vh] pb-10 bg-black'>
           {/* background image */}
-          <div className='w-full h-[50%] lg:h-[100%] bg-size-[100%_100%] bg-no-repeat bg-[url("assets/resources/bg-cafe-sm.jpg")] lg:bg-[url("assets/resources/bg-cafe-lg.jpg")] lg:bg-[url("assets/resources/bg-cafe-lg.jpg")]'></div>
+          <div className='w-full h-[50%] lg:h-[100%]'>
+            {screen.width <= 640 && <img fetchPriority='high' src={smImage} width={screen.width} height={screen.height/3}   alt='caffe image' />}
+            {screen.width <= 1024 && <img fetchPriority='high' src={lgImage} width={screen.width} height={screen.height/3} alt='caffe image' />}
+            {screen.width >= 1025 && <img fetchPriority='high' src={xlImage} width={screen.width} height={screen.height/3} alt='caffe image' />}
+          </div>
           {/* coffee list container */}
           <CoffeeList />
         </div>
